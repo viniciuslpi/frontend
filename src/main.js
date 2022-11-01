d9f9cdd3165390987d79a081784ddc0be2e92d3b
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-11-01 17:51:49 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+
+import App from './App.vue'
+const myApp = createApp(App)
+
+myApp.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+})
+
+myApp.mount('#app')
