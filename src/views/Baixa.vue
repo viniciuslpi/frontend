73af388122a1f7d379d0f8db0eabf68f7acf23f6
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-11-10 18:17:14 -0300

    novas correçoes - com bug

--- src/views/Baixa.vue
+++ src/views/Baixa.vue
@@ -2,8 +2,8 @@
   <div class="q-pa-md">
     <q-form @submit="onSubmit" class="q-gutter-md" style="width: 50%;margin: auto;">
 
-      <q-select behavior="menu" outlined v-model="input.entidade" use-input hide-selected fill-input input-debounce="200"
-        :options="options" @filter="filterFn" @update:model-value="updateFn"
+      <q-select behavior="menu" outlined v-model="input.entidade" use-input hide-selected fill-input
+        input-debounce="200" :options="options" @filter="filterFn" @update:model-value="updateFn"
         hint="Minimum 3 characters to trigger filtering" style="width: 100%; padding-bottom: 32px" label="Entidade">
         <template v-slot:no-option>
           <q-item>
@@ -41,7 +41,7 @@ export default {
       try {
         const response = await axios.get(`/api/recepcao/v1/entidades`);
         const arrayEntidades = response.data.rows;
-        arrayEntidades.map(item => stringOptions.push(item.nome))
+        arrayEntidades.map(item => stringOptions.push(item))
       } catch (err) {
         console.log(err);
       }
@@ -53,7 +53,7 @@ export default {
 
       updateFn(val) {
         if (val && typeof val === "object") {
-          console.log(val)
+          // console.log(val)
           input.value.entidade = val.label
         }
       },
@@ -66,29 +66,85 @@ export default {
 
         update(async () => {
           const needle = val.toLowerCase()
-          options.value = stringOptions.filter(v => v.toLowerCase().indexOf(needle) > -1);
+          options.value = stringOptions.filter(v => v.nome.toLowerCase().indexOf(needle) > -1);
+          console.log(options.value)
         })
 
       },
 
-      onSubmit() {
+      async onSubmit() {
+
         const formData = {
           entidade: input.value.entidade,
           atendimento: input.value.numAtendimento
         }
 
+        // if (formData.entidade == null || formData.atendimento == null) {
+        //   console.log(">>> Os campos precisam estar preenchidos");
+        // } else {
 
-        if (input.value.entidade == null || input.value.numAtendimento == null) {
-          console.log(">>> Os campos precisam estar preenchidos");
-        } else {
-          console.log(">>> Baixa de Senha Realizada");
-          console.log(formData);  
-          input.value.numAtendimento = null
+
+        try {
+          let resp = await axios.get(`/api/recepcao/v1/atendimentos/${formData.atendimento}`);
+
+
+          if (resp.data) {
+            if (resp.status === 200) {
+              let atendimento = resp.data;
+              // console.log(atendimento);
+              let pessoa = await axios.get(`/api/recepcao/v1/pessoas/${atendimento.pessoa_id}`);
+              console.log(pessoa);
+              if (atendimento.entidade_id) {
+                console.log("Atendimento já baixado", "");
+              } else {
+                if (!formData.atendimento || formData.atendimento <= 0) {
+                  console.log(
+                    "Id inválido",
+                    "Favor entrar em contato com o suporte"
+                  );
+                } else {
+                  // let atendimentoUpdate = await axios.put(`/api/recepcao/v1/atendimentos/${formData.atendimento}`, { entidade_id: this.fields.entidade });
+
+                  // if (atendimentoUpdate.status != 204) {
+                  //   console.error("ERRO NA GRAVAÇÃO");
+                  //   console.log(
+                  //     "Erro na gravação dos dados!",
+                  //     "Favor entrar em contato com o suporte"
+                  //   );
+                  // } else {
+                  //   this.fields.id = "";
+                  //   this.ok.show = true;
+                  //   this.ok.data = moment(atendimento.data).format("DD/MM/YYYY");
+                  //   this.ok.nome = pessoa.data.nome;
+                  //   this.ok.senha = atendimento.senha;
+
+
+                  console.log("teste");
+
+                  // this.okMessage = `Atendimento realizado em <b>${moment(atendimento.data).format('DD/MM/YYYY')}</b> para ${pessoa.data.nome}, número de Senha: ${atendimento.senha}`;
+
+                  // this.setTimeoutMessage();
+                }
+              }
+            }
+          }
+        } catch (err) {
+          if (err.response) {
+            if (err.response.status === 404) {
+              console.log("Atendimento não encontrado", "");
+            }
+          }
         }
-      },
 
+        // console.log(">>> Baixa de Senha Realizada");
+        // console.log(formData);
+        // input.value.numAtendimento = null
+        // }
+      }
 
     }
   }
 }
+
+
 </script>
